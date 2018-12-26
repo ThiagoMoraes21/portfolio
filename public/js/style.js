6bb945155f18dc68e1ab84439216c3b6f5990db2
Thiago Moraes <thiago4x70@gmail.com>
2018-12-26 20:09:23 -0200

    Make the website responsive

--- public/js/style.js
+++ public/js/style.js
@@ -1,1 +1,11 @@
 console.log('CONNECTED!');
+
+new TypeIt('.type-it', {
+  strings: ['Web', 'Front-End', 'Back-End'],
+  cursorSpeed: 1200,
+  speed: 220,
+  nextStringDelay: 750,
+  autoStart: false,
+  breakLines: false,
+  loop: true
+});
